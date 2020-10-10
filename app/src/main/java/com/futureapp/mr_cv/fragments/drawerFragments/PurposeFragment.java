9b07d0023f904b2e851066f323376f7556f33fb9
mohammad-alrmamneh <mohammad_alrmamneh_10@yahoo.com>
2020-10-11 02:27:28 +0300

    add PurposeFragment data and design (workrd)

--- app/src/main/java/com/futureapp/mr_cv/fragments/drawerFragments/PurposeFragment.java
+++ app/src/main/java/com/futureapp/mr_cv/fragments/drawerFragments/PurposeFragment.java
@@ -6,14 +6,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.futureapp.mr_cv.R;
+import com.futureapp.mr_cv.util.Global;
+import com.hanks.htextview.evaporate.EvaporateTextView;
+import com.hanks.htextview.typer.TyperTextView;
 
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class PurposeFragment extends Fragment {
 
     public static final String ARG_PAGE = "ARG_PAGE";
+
+    @BindView(R.id.text_TTV)
+    TyperTextView textTTV;
+    @BindView(R.id.title_ETV)
+    EvaporateTextView titleETV;
+
 
     public static PurposeFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -34,6 +44,32 @@ public class PurposeFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setData();
+    }
+
+    private void setData() {
+
+        StringBuilder sb = new StringBuilder();
+        String title = "";
+        String[] textSplit = Global.configFirebaseModel.getPurpose().split("::");
+
+        for (int i = 0; i < textSplit.length; i++) {
+            if (i == 0) {
+                title = textSplit[i];
+            } else {
+                sb.append(textSplit[i]);
+            }
+        }
+
+        titleETV.animateText(title);
+        textTTV.animateText(sb);
+
     }
 
 }
